971269d84289723d31f728f5357e773fbff6e807
조예지 <celpegor216@gmail.com>
2024-03-14 11:51:18 +0900

    feat: set initial project structure

---
+++ day03_clean_architecture/hands_on_2/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "hands_on_2"
+include(":app")
